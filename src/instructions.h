6c3d31cc23690ec8b1dfff16534064cb80d8b938
ryan-rashidian <ryanrashidian@gmail.com>
2026-06-12 00:23:38 -0700

    Add header for instruction enums

---
+++ src/instructions.h
@@ -0,0 +1,31 @@
+/*
+ * Instruction set for the VM
+ */
+
+#ifndef INSTRUCTIONS_H
+#define INSTRUCTIONS_H
+
+enum {
+    INSTR_NOP,
+    INSTR_PUSH,
+    INSTR_POP,
+    INSTR_ADD,
+    INSTR_SUB,
+    INSTR_MUL,
+    INSTR_EQ,
+    INSTR_LT,
+    INSTR_GT,
+    INSTR_AND,
+    INSTR_OR,
+    INSTR_XOR,
+    INSTR_JMP,
+    INSTR_JMPZ,
+    INSTR_JMPNZ,
+    INSTR_DUP,
+    INSTR_SWAP,
+    INSTR_HALT,
+    INSTR_COUNT
+};
+
+#endif
+
